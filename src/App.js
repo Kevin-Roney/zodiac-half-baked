bb83dfe25fa57a4625967ecc5b220ed7e874975b
Kevin Roney <ktroney.pdx@gmail.com>
2022-04-25 15:14:51 -0700

    got zodiac plates rendering and appending properly, time to add some more css

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import './App.css';
 {/* import the zodiac array */}
 import Header from './Header/Header.js';
 import Footer from './Footer/Footer.js';
+import { zodiacs } from './data.js';
+import Main from './Main/Main.js';
+
 
 function App() {
   return (
     <div className="App">
       {/* import and use the Header. Main, and Footer components here */}
-      <Header name="Kevin"/>
       {/* the Header component needs a string passed in as the `name` prop */}
+      <Header name="Kevin"/>
+      <Main
+        zodiacs={zodiacs}
+      />
       {/* the Footer component needs a year passed in as the `year` prop */}
       <Footer year="2022"/>
       {/* pass the zodiac array imported above to the Main component as a `zodiacs` prop */}
